ec200a3e80ee995c3a87c63fea490461f53d154f
Joseval Viana <josevalviana@gmail.com>
2011-07-29 09:18:07 -0300

    provided some field checking

--- com_hospitals/admin/tables/hospital.php
+++ com_hospitals/admin/tables/hospital.php
@@ -25,4 +25,50 @@ class HospitalsTableHospital extends JTable {
 		
 		return parent::store($updateNulls);
 	}
+	
+	/**
+	 * Overloaded check function
+	 * 
+	 * @return boolean
+	 * @see JTable::check
+	 * @since 1.5
+	 */
+	function check() {
+		if (JFilterInput::checkAttribute(array('href', $this->webpage))) {
+			$this->setError(JText::_('COM_HOSPITALS_WARNING_PROVIDE_VALID_URL'));
+			return false;
+		}
+		
+		// check for http, https, ftp on webpage
+		if ((strlen($this->webpage) > 0)
+			&& (stripos($this->webpage, 'http://') === false)
+			&& (stripos($this->webpage, 'https://') === false)
+			&& (stripos($this->webpage, 'ftp://') === false)) {
+			$this->webpage = 'http://'.$this->webpage;
+		}
+		
+		/** check for valid name */
+		if (trim($this->name) == '') {
+			$this->setError(JText::_('COM_HOSPITALS_WARNING_PROVIDE_VALID_NAME'));
+			return false;
+		}
+		
+		/** check for existing name */
+		$query = 'SELECT id FROM #__hospitals WHERE name = '.$this->_db->Quote($this->name).' AND catid = '.(int) $this->catid;
+		$this->_db->setQuery($query);
+		
+		$xid = intval($this->_db->loadResult());
+		if ($xid && $xid != intval($this->id)) {
+			$this->setError(JText::_('COM_HOSPITALS_WARNING_SAME_NAME'));
+			return false;
+		}
+		
+		/** check for valid category */
+		if (trim($this->catid) == '') {
+			$this->setError(JText::_('COM_HOSPITALS_WARNING_CATEGORY'));
+			return false;
+		}
+		
+		return true;
+	}
 }
