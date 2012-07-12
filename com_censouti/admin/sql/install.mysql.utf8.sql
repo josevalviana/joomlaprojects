01adab63acd19590c61066838d84a4f9aafc800d
Joseval Viana <josevalviana@gmail.com>
2012-07-12 14:59:52 -0300

    criado novos campos no db censouti

--- com_censouti/admin/sql/install.mysql.utf8.sql
+++ com_censouti/admin/sql/install.mysql.utf8.sql
@@ -4,12 +4,15 @@ CREATE TABLE `#__censouti` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT,
 	`sisreg` INTEGER NOT NULL,
 	`nome` VARCHAR(255) NOT NULL,
+	`hospital_id` INT NOT NULL default '0',
+	`admissao` date NOT NULL default '0000-00-00',
+	`diagnostico` VARCHAR(255) NOT NULL,
+	`evolucao` TINYINT(1) NOT NULL default '0',
+	`alta` TINYINT(1) NOT NULL default '0',
+	`dt_alta` date NOT NULL default '0000-00-00',
 	`created` datetime NOT NULL default '0000-00-00 00:00:00',
     `created_by` int(10) unsigned NOT NULL default '0',
     `modified` datetime NOT NULL default '0000-00-00 00:00:00',
     `modified_by` int(10) unsigned NOT NULL default '0',
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `#__censouti` (`sisreg`, `nome`, `created`, `created_by`) VALUES (12345, 'CLAUDIO', NOW(), 76);
-INSERT INTO `#__censouti` (`sisreg`, `nome`, `created`, `created_by`) VALUES (54321, 'MESSIAS', NOW(), 76);
