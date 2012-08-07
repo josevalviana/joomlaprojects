69760717830c558b12fc57e7ef5a3bce01ca808b
Joseval Viana <josevalviana@gmail.com>
2012-08-07 14:53:03 -0300

    impedido valor zero para alguns campos obrigatórios

--- com_filauti/admin/tables/paciente.php
+++ com_filauti/admin/tables/paciente.php
@@ -36,4 +36,33 @@ class FilaUtiTablePaciente extends JTable
 		
 		return parent::store($updateNulls);
 	}
+        
+        function check()
+        {                          
+            // checa campo obrigatório MORTE ENCEFÁLICA
+            if ((int) $this->mencef == 0) {
+                $this->setError(JText::sprintf(JText::_('COM_FILAUTI_FIELD_REQUIRED'), JText::_('COM_FILAUTI_FIELD_MENCEF_LABEL')));
+                return false;
+            }
+            
+            // checa campo obrigatório HEMODIALISE
+            if ((int) $this->hemodialise == 0) {
+                $this->setError(JText::sprintf(JText::_('COM_FILAUTI_FIELD_REQUIRED'), JText::_('COM_FILAUTI_FIELD_HEMODIALISE_LABEL')));
+                return false;
+            }
+            
+            // checa campo obrigatório ISOLAMENTO
+            if ((int) $this->isolamento == 0) {
+                $this->setError(JText::sprintf(JText::_('COM_FILAUTI_FIELD_REQUIRED'), JText::_('COM_FILAUTI_FIELD_ISOLAMENTO_LABEL')));
+                return false;
+            }
+            
+            // checa campo obrigatório POSOP
+            if ((int) $this->posop == 0) {
+                $this->setError(JText::sprintf(JText::_('COM_FILAUTI_FIELD_REQUIRED'), JText::_('COM_FILAUTI_FIELD_POSOP_LABEL')));
+                return false;
+            }
+            
+            return true;
+        }
 }
