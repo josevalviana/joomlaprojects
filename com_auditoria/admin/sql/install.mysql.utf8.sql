b20c7c34ee49a486ece63e51318fc9ee62bbbddd
Joseval Viana <josevalviana@gmail.com>
2012-12-04 13:26:51 -0300

    intercorrencia do com_auditoria mantem formatação

--- com_auditoria/admin/sql/install.mysql.utf8.sql
+++ com_auditoria/admin/sql/install.mysql.utf8.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `#__auditoria` (
     `hospital` INTEGER unsigned NOT NULL default '0', 
     `turno` INTEGER unsigned NOT NULL default '0',
     `telefone` VARCHAR(255) default NULL,
-    `intercorrencia` text default NULL,
+    `intercorrencia` mediumtext default NULL,
     `created` datetime NOT NULL default '0000-00-00 00:00:00',
     `created_by` int(10) unsigned NOT NULL default '0',
     `modified` datetime NOT NULL default '0000-00-00 00:00:00',
